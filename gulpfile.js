658e2cddea3297241b48e85a692cddb5a34745a9
Cristian J. Ambrosi <cris_ambrosi@hotmail.com>
2019-05-13 00:18:33 -0300

    Configurando gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -5,18 +5,27 @@ const jsmin  = require('gulp-jsmin');
 const rename = require('gulp-rename');
 const uglifycss = require('gulp-uglifycss');
 const browserSync = require('browser-sync');
+const notify = require('gulp-notify');
+
+let reload = browserSync.reload;
 
 let sources = {
     lib: [
         './node_modules/jquery/dist/jquery.min.js',
         './node_modules/chart.js/dist/Chart.min.js'
     ],
-    less: [
-        './assets/dev/less/reset.less',
-        './assets/dev/less/default.css'
-    ],
     js: [
         './assets/dev/js/default.js'
+    ],
+    less: [
+        './assets/dev/less/reset.less',
+        './assets/dev/less/default.less'
+    ],
+    dir_css: [
+        './assets/dev/less/**'
+    ],
+    dir_js: [
+        './assets/dev/js/**'
     ],
     server: {
         url: 'localhost',
@@ -24,44 +33,48 @@ let sources = {
     }
 }
 
-// gulp.task('w', function() {
-//     gulp.watch('./assets/dev/**/*', [
-//         'css',
-//         'js'
-//     ]);
-// });
-
-gulp.task('css', function() {
+function css() {
     return gulp.src(sources.less)
-    .pipe(concat('build.css'))
-    .pipe(uglifycss())
-    .pipe(rename({
-        suffix: '.min'
-    }))
-    .pipe(gulp.dest('./assets/pub/css/'));
-});
+        .pipe(concat('build.css'))
+        .pipe(less())
+        .pipe(uglifycss())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('./assets/pub/css/'));
+}
 
-gulp.task('lib', function() {
+function lib() {
     return gulp.src(sources.lib)
-    .pipe(concat('lib.js'))
-    .pipe(gulp.dest('./assets/pub/js/'));
-});
+        .pipe(concat('lib.js'))
+        .pipe(gulp.dest('./assets/pub/js/'));
+}
 
-gulp.task('js', function() {
+function js() {
     return gulp.src(sources.js)
-    .pipe(concat('build.js'))
-    .pipe(jsmin())
-    .pipe(rename({
-        suffix: '.min'
-    }))
-    .pipe(gulp.dest('./assets/dev/pub/js/'));
-});
+        .pipe(concat('build.js'))
+        .pipe(jsmin())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('./assets/pub/js/'));
+}
 
-gulp.task('browser-sync', function () {
+function browser_sync(done) {
     browserSync.init({
         proxy: sources.server.url,
         port: sources.server.port
     });
-});
 
-// gulp.task('default', ['css']);
+    done();
+}
+
+function watchFiles() {
+    gulp.watch(sources.dir_css, css);
+    gulp.watch(sources.dir_js, gulp.series(js, reload));
+    gulp.src('./assets/pub/js/' + 'build.min.js')
+        .pipe(notify({ message: 'Gulp is Watching. Happy Coding!' }));
+}
+
+gulp.task('css', css);
+gulp.task('lib', lib);
+gulp.task('js', js);
+gulp.task('watchFiles', watchFiles);
+// gulp.task('dev', dev);
+gulp.task('default', gulp.series(watchFiles, browser_sync));
