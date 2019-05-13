9a21934782b2b642970e6ca6c29a91724b9fc5e5
Cristian J. Ambrosi <cristian.ambrosi@compasso.com.br>
2019-05-13 18:34:33 -0300

    Refeito o gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,19 @@
 const gulp  = require('gulp');
 const less  = require('gulp-less');
-const concat = require('gulp-concat');
 const jsmin  = require('gulp-jsmin');
+const concat = require('gulp-concat');
 const rename = require('gulp-rename');
+// const notify = require('gulp-notify');
 const uglifycss = require('gulp-uglifycss');
-const browserSync = require('browser-sync');
-const notify = require('gulp-notify');
+const browserSync = require('browser-sync').create();
 
 let reload = browserSync.reload;
 
 let sources = {
+    less: [
+        './assets/dev/less/reset.less',
+        './assets/dev/less/default.less'
+    ],
     lib: [
         './node_modules/jquery/dist/jquery.min.js',
         './node_modules/chart.js/dist/Chart.min.js'
@@ -17,64 +21,76 @@ let sources = {
     js: [
         './assets/dev/js/default.js'
     ],
-    less: [
-        './assets/dev/less/reset.less',
-        './assets/dev/less/default.less'
-    ],
-    dir_css: [
-        './assets/dev/less/**'
-    ],
-    dir_js: [
-        './assets/dev/js/**'
-    ],
+    watch: {
+        less: [
+            './assets/dev/less/**/*.less'
+        ],
+        js: [
+            './assets/dev/js/**/*.js'
+        ],
+        html: [
+            './*.html'
+        ]
+    },
+    path: {
+        css: [
+            './assets/pub/css/'
+        ],
+        js: [
+            './assets/pub/js/'
+        ]
+    },
     server: {
         url: 'localhost',
         port: '8088'
     }
 }
 
-function css() {
-    return gulp.src(sources.less)
-        .pipe(concat('build.css'))
-        .pipe(less())
-        .pipe(uglifycss())
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(gulp.dest('./assets/pub/css/'));
-}
-
 function lib() {
     return gulp.src(sources.lib)
         .pipe(concat('lib.js'))
-        .pipe(gulp.dest('./assets/pub/js/'));
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest(sources.path.js));
 }
 
-function js() {
+function script() {
     return gulp.src(sources.js)
         .pipe(concat('build.js'))
         .pipe(jsmin())
         .pipe(rename({ suffix: '.min' }))
-        .pipe(gulp.dest('./assets/pub/js/'));
+        .pipe(gulp.dest(sources.path.js));
 }
 
-function browser_sync(done) {
+function style() {
+    return gulp.src(sources.less)
+        .pipe(concat('build.css'))
+        // .pipe(less().on('error', less.logError))
+        .pipe(less())
+        .pipe(uglifycss())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest(sources.path.css))
+        .pipe(browserSync.stream());
+}
+
+function browserSyncServer() {
     browserSync.init({
+        baseDir: './',
         proxy: sources.server.url,
         port: sources.server.port
     });
-
-    done();
 }
 
-function watchFiles() {
-    gulp.watch(sources.dir_css, css);
-    gulp.watch(sources.dir_js, gulp.series(js, reload));
-    gulp.src('./assets/pub/js/' + 'build.min.js')
-        .pipe(notify({ message: 'Gulp is Watching. Happy Coding!' }));
+function watchFile() {
+    gulp.watch(sources.watch.less, style);
+    gulp.watch(sources.watch.html).on('change', reload);
+    gulp.watch(sources.watch.js).on('change', reload);
 }
 
-gulp.task('css', css);
-gulp.task('lib', lib);
-gulp.task('js', js);
-gulp.task('watchFiles', watchFiles);
-// gulp.task('dev', dev);
-gulp.task('default', gulp.series(watchFiles, browser_sync));
+exports.lib = lib;
+exports.style = style;
+exports.script = script;
+exports.watchFile = watchFile;
+exports.browserSyncServer = browserSyncServer;
+
+// gulp.task('dev', gulp.series(style, lib, script));
+gulp.task('default', gulp.parallel(style, watchFile, browserSyncServer));
