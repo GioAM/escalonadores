928742bc9338de4d40216e24f095d5d970c3955a
giovani.meneguel <gio_meneguel@hotmail.com>
2019-05-27 10:44:00 -0300

    Ajuste de mensagem

--- assets/dev/js/default.js
+++ assets/dev/js/default.js
@@ -25,7 +25,7 @@ function createQueue(){
 	var newJob =  new JobStruct(id,$('#clocks').val(),$('.prioritySelect').val());
 	jobsToExecute.push(newJob);
 	id++;
-	$('.table-logs').append("<li class='-itemjob'><i class='fas fa-level-up-alt -arrowjobicon'></i>Job <span class='-numberjob'>" + newJob.jobId + "</span> adicionado a fila <i class='fas fa-minus -minusarrowicon'></i> Total de execução: <span class='-numbersecondjob'>" + newJob.totalClocks +"</span> segundo(s) </li>");
+	$('.table-logs').append("<li class='-itemjob'><i class='fas fa-level-up-alt -arrowjobicon'></i>Job <span class='-numberjob'>" + newJob.jobId + "</span> adicionado a fila <i class='fas fa-minus -minusarrowicon'></i> Total de execução: <span class='-numbersecondjob'>" + newJob.totalClocks +"</span> Hz </li>");
 }
 
 function startJobs() {
